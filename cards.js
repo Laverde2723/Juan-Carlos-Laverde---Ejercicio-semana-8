7e35d01bbad72523224c7985d938a953fcf27884
laverde 2723 <juanklaverde03@gmail.com>
2023-03-24 08:39:41 -0500

    Juan Carlos: :D1

---
+++ cards.js
@@ -0,0 +1,103 @@
+[
+    {
+     name: "Camisa manga corta efecto piel",
+     url: "16755f79331335f322be05969268acbf-1130450400_2_4_0.webp",
+     description: "Ref 1130/450/400",
+     price : "249.000COP",
+     sizes : ["XS", "S", "M", "L", "XL"],
+     color : "Azul",
+     category : "Camisas"
+
+    },
+
+    {
+        name: "Camisa manga corta solapas",
+        url: "320f7f693bf90a817ca6e091628b7011-1139405800_2_4_0.webp",
+        description: "Ref 1139/405/800",
+        price : "229.000COP",
+        sizes : ["XS", "S", "M", "L", "XL"],
+        color : "Negro",
+        category : "Camisas"
+    },
+
+    {
+        name: "Camisa manga corta sarga rayas",
+        url: "1c3782772c2778f70edf17ed180293ee-1108019060_2_4_0.webp",
+        description: "Ref 1108/019/060",
+        price : "169.900COP",
+        sizes : ["XXS", "XS", "S", "M", "L", "XL", "XXL"],
+        color : "Negro/Blanco",
+        category : "Camisas" 
+    },
+
+    {
+        name: "Sobrecamisa Básica",
+        url: "0ce783c147b89324d67db7db9adcb162-6540665802_2_4_0.webp",
+        description: "Ref 6540/665/802",
+        price : "159.900COP",
+        sizes : ["XS", "S", "M", "L", "XL"],
+        color : "Gris",
+        category : "Camisas" 
+    },
+
+    {
+        name: "Camisa manga corta print",
+        url: "b3f0b6c66814bcb047979f2bc134e3c0-6550777251_2_4_0.webp",
+        description: "Ref 6550/777/251",
+        price : "199.900COP",
+        sizes : ["XS", "S", "M", "L", "XL"],
+        color : "Blanco roto",
+        category : "Camisas" 
+    },
+
+    {
+        name: "Camisa manga corta bordado tigre",
+        url: "051cfa9d5b9182042a8dd4c31d16b113-1116954250_2_4_0.webp",
+        description: "Ref 1116/954/250",
+        price : "309.000COP",
+        sizes : ["XS", "S", "M", "L", "XL"],
+        color : "Blanco",
+        category : "Camisas" 
+    },
+
+    {
+        name: "Camisa manga corta print paisley",
+        url: "1d861e46a15f3fd61865056f987848b4-1134557060_2_4_0.webp",
+        description: "Ref 1134/557/060",
+        price : "119.900COP",
+        sizes : ["XS", "S", "M", "L", "XL"],
+        color : "Blanco/Negro",
+        category : "Camisas" 
+    },
+
+    {
+        name: "Camiseta manga corta oversize print",
+        url: "1963b9ea60038c0dc74a3e6291a43b11-2336987818_2_4_0.webp",
+        description: "Ref 2336/987/818",
+        price : "129.900COP",
+        sizes : ["XS", "S", "M", "L", "XL"],
+        color : "Blanco roto",
+        category : "Camisaseta" 
+    },
+
+    {
+        name: "Camiseta manga corta boxy fit print",
+        url: "14f629b5d71a68c1a90a35ecee50ccc9-2309538250_2_4_0.webp",
+        description: "Ref 2309/538/250",
+        price : "109.900COP",
+        sizes : ["XS", "S", "M", "L", "XL"],
+        color : "Blanco",
+        category : "Camiseta" 
+    },
+
+    {
+        name: "Camiseta manga corta boxy fit print Tupac",
+        url: "16be93250678af227b9e8e400fd38c1d-2406851250_2_4_0.webp",
+        description: "Ref 2406/851/250",
+        price : "109.900COP",
+        sizes : ["XS", "S", "M", "L", "XL"],
+        color : "Blanco",
+        category : "Camiseta" 
+    },
+
+]
